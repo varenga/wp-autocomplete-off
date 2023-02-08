da6b89d97280d993a280858b86f94b03d8d9cbc4
Xose de la Paz <varenga@users.noreply.github.com>
2023-02-08 17:03:27 +0100

    Add files via upload

--- wp-autocomplete-off.php
+++ wp-autocomplete-off.php
@@ -3,20 +3,32 @@
 Plugin Name: WP Autocomplete Off
 Plugin URI: https://github.com/varenga/wp-autocomplete-off
 Description: Permite quitar el autocompletado del formulario de acceso a Wordpress
-Version: 1.0.1
+Version: 1.0.5.0
 Author: Xose de la Paz
 Author URI: https://pikselin.es
 License: GPL
 */
 
+/*
 function wpse_159462_login_form() {
     echo <<<html
-<script>
-    document.getElementById( "user_login" ).autocomplete = "off";
-    document.getElementById( "user_pass" ).autocomplete = "off";
-</script>
-html;
+	<script>
+	    document.getElementById( "user_login" ).autocomplete = "nope";
+	    document.getElementById( "user_pass" ).autocomplete = "nope";
+	    document.getElementById( "loginform" ).autocomplete = "off";
+	</script>
+	html;
 }
 
 add_action( 'login_form', 'wpse_159462_login_form' );
+*/
+
+function autocomplete_script() {
+    wp_enqueue_script( 'js-autocomplete', plugin_dir_url( __FILE__ ) . 'js/functions.js', array(), '1.0.0', true);
+}
+
+add_action('wp_enqueue_scripts', 'autocomplete_script');
+add_action('admin_enqueue_scripts', 'autocomplete_script');
+add_action('login_enqueue_scripts', 'autocomplete_script');
+
 ?>
